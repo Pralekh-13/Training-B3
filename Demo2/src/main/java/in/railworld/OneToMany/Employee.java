7cfb0ebcec50ec83f2209793cf7412c7a42cb510
PRALEKH RAMBHAD <106136815+Pralekh-13@users.noreply.github.com>
2024-06-13 16:08:52 +0530

    hibernate with mapping

---
+++ Demo2/src/main/java/in/railworld/OneToMany/Employee.java
@@ -0,0 +1,22 @@
+package in.railworld.OneToMany;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
